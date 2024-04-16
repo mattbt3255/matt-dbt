ee8dc8037d920c53800fc6791388d268e5511779
Matt Brown <matt.brown@flocksafety.com>
2024-04-16 09:46:44 -0500

    Adding seeds and analysis

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,5 +1,18 @@
 with customers as (
   select * from {{ ref('stg_customers') }}
+),
+
+employees as (
+  select * from {{ ref('employees' ) }}
+),
+
+final as (
+  select
+    customers.*,
+    employees.employee_id is not null as is_employee
+  
+  from customers
+  left join employees on employees.customer_id = customers.customer_id
 )
 
-select * from customers
+select * from final
