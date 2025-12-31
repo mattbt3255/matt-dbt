dfe1c468937b69848e47c9527509e6d2740c7ff9
Matt Brown <matt.brown@flocksafety.com>
2025-12-31 15:51:04 -0600

    Creating folder just for duckdb setup

--- analyses/other/create_schema_jaffle_shop.sql
+++
@@ -1,8 +0,0 @@
--- Create the schema
-CREATE SCHEMA raw;
-
--- Load all three files
-CREATE TABLE raw.customers AS SELECT * FROM read_csv_auto('seeds/customers.csv');
-CREATE TABLE raw.orders AS SELECT * FROM read_csv_auto('seeds/orders.csv');
-CREATE TABLE raw.payments AS SELECT * FROM read_csv_auto('seeds/payments.csv');
-.exit
