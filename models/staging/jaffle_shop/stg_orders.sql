dd95320a31510d3e31f68ba21639e2f50caf8903
Matt Brown <matt.brown@flocksafety.com>
2025-12-31 15:36:12 -0600

    Unit test updates and figuring out how to query duckdb in datagrip

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,10 +1,17 @@
 select
+  -- Native Fields --
   id as order_id,
   user_id as customer_id,
   order_date,
-  status
+  status,
+
+  -- Derived Fields --
+  status like '%pending%' as is_status_pending,
+
+  case
+    when status like '%pending%' then 'placed'
+    when status like '%return%' then 'returned'
+    else status
+  end as status_cleaned
   
 from {{ source('jaffle_shop', 'orders') }}
-
-{{ limit_data_in_dev('order_date', '7') }}
-
